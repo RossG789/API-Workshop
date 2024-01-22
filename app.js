6e255430ebcadcb11f2a61729cfcf4753731f09b
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-22 14:07:38 +0000

    second

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   //   console.log(e);
   let query = e.target.input.value;
+
   search(query);
 });
+
+function clearGallery() {}
 
 async function search(queryParam) {
   let response = await fetch(
     `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=_yyXBx_Z41nrX0ItYX2TcUQUYVDo_6Lv4gT0hwxwhTI`
   );
-  //   console.log(response);
 
   let data = await response.json();
+  createImages(data.results);
+}
 
-  let img = document.createElement("img");
-  img.src = data.results[0].urls.raw;
-  imgcontainer.appendChild(img);
+function createImages(arrayImages) {
+  arrayImages.forEach((img) => {
+    let imgTag = document.createElement("img");
+    imgTag.src = img.urls.raw;
+    imgcontainer.appendChild(imgTag);
+  });
 }
